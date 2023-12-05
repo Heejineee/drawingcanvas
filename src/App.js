a5e97c21f91df4d1f6ce11301f888e78d56c236c
grp-pineone-at-819068063220 <heejinee6@gmail.com>
2023-12-05 11:17:00 +0900

    canvas update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const canvasInformRef = useRef({
+    width: 0,
+    height: 0,
+    pixelRatio: window.devicePixelRatio > 1 ? 2 : 1,
+  });
+
+  const canvasSizeSetting = (width, height) => {
+    if (!canvasRef.current) return;
+
+    const resultWidth = width * canvasInformRef.current.pixelRatio;
+    const resultHeight = height * canvasInformRef.current.pixelRatio;
+    canvasRef.current.width = resultWidth;
+    canvasRef.current.height = resultHeight;
+    canvasInformRef.current.width = resultWidth;
+    canvasInformRef.current.height = resultHeight;
+  };
+
+  const canvasContextSetting = (context) => {
+    if (!context) return;
+    context.scale(
+      canvasInformRef.current.pixelRatio,
+      canvasInformRef.current.pixelRatio
+    );
+    context.lineCap = "round";
+    context.strokeStyle = "black";
+    context.lineWidth = 3;
+    contextRef.current = context;
+  };
+
+  const init = () => {
+    if (!canvasRef.current) return;
+    const { innerWidth, innerHeight } = window;
+    canvasSizeSetting(innerWidth, innerHeight);
+    const getContext = canvasRef.current.getContext("2d");
+    if (!getContext) return;
+    canvasContextSetting(getContext);
+  };
+
+  const handlePointerDown = ({ nativeEvent }) => {
+    setIsDrawing(true);
+    if (!contextRef.current) return;
+
+    const { offsetX, offsetY } = nativeEvent;
+
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+  };
+
+  const handlePointerMove = ({ nativeEvent }) => {
+    if (!isDrawing || !contextRef.current) return;
+
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+  };
+
+  const handlePointerUp = () => {
+    setIsDrawing(false);
+    if (!contextRef.current) return;
+    contextRef.current.closePath();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <canvas
+        ref={canvasRef}
+        onPointerDown={handlePointerDown}
+        onPointerMove={handlePointerMove}
+        onPointerUp={handlePointerUp}
+      ></canvas>
     </div>
   );
 }
